323e17602bdc2bd60bf0083becaa70d85b0fc168
Turbstructor <ycw0201@naver.com>
2024-01-23 20:02:44 +0900

    Feat: add divide-by-zero situation in Divider

--- app/src/main/java/fun/turbstructor/workshop/operator/Divider.java
+++ app/src/main/java/fun/turbstructor/workshop/operator/Divider.java
@@ -3,6 +3,7 @@ package fun.turbstructor.workshop.operator;
 public class Divider {
     public static int divide(int a, int b)
     {
+        if(b == 0) return 0;
         return a / b;
     }    
 }
