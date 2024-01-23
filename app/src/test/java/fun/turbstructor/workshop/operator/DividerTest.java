f0f83e5d0431a2923cbfd871445500a45ce890ec
Turbstructor <ycw0201@naver.com>
2024-01-23 20:02:17 +0900

    Test: add divide-by-zero test

--- app/src/test/java/fun/turbstructor/workshop/operator/DividerTest.java
+++ app/src/test/java/fun/turbstructor/workshop/operator/DividerTest.java
@@ -9,4 +9,11 @@ public class DividerTest {
     {
         assertEquals(Divider.divide(50, 2), (50 / 2));
     }
+
+    @Test
+    void DividerDoesNotThrowException()
+    {
+        int result = assertDoesNotThrow(() -> { return Divider.divide(10, 0); });
+        assertEquals(result, 0);
+    }
 }
